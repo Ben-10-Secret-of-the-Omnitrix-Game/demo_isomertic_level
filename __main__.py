c3661a14ab3625d985b6cc9b68a7e135c5b6612a
Dmitry Molokovich <molokovich.dima@gmail.com>
2021-01-14 18:57:43 +0300

    implemented isometric rendering

--- __main__.py
+++ __main__.py
@@ -1,47 +1,101 @@
 import pygame
 import sys
+import os
+
 from .tile import Tile
 
 MAPWIDTH = 3
-MAPHEIGH = 3
+MAPHEIGH = 4
 TILESIZE = 50
+
+wall_graphic_height = 98
+floor_graphic_wdth = 103
+floor_graphic_height = 53
+wall_height = wall_graphic_height - floor_graphic_height
+border_offset = 150
 
 
 BLACK = (0, 0, 0)
 BROWN = (153, 76, 0)
 GREEN = (0, 255, 0)
-BLUE  = (0, 0, 255)
-
-surface = object
-tilemap = [
-    [Tile(BROWN, 0, 0), Tile(BROWN, 1, 0), Tile(BROWN, 2, 0)],
-    [Tile(BROWN, 0, 1), Tile(BROWN, 1, 1), Tile(BROWN, 2, 1)],
-    [Tile(BROWN, 0, 2), Tile(BROWN, 1, 2), Tile(BROWN, 2, 2)],
-]
+BLUE = (0, 0, 255)
 
 
-def render_tilemap():
-    global surface
-    for x in range(MAPHEIGH):
-            for y in range(MAPWIDTH):
-                tile = tilemap[y][x]
-                tile: Tile
-                print(x, y)
-                pygame.draw.rect(surface, tile.color, (x * TILESIZE, y * TILESIZE, TILESIZE, TILESIZE))
+def load_resource_image(name):
+    path = os.path.join("demo_isometric_level/resources", f"{name}.png")
+    print(path)
+    image = pygame.image.load(path)
+    # TODO remove convert() and collide all pngs' into single one for performace purposes
+    image.convert()
+    return image
+
+
+game_screen = object
+wall = object
+floor = object
+
+tilemap = []
+
+
+def render_basic_tilemap():
+    global game_screen
+    for y in range(MAPWIDTH):
+        for x in range(MAPHEIGH):
+            tile = tilemap[x][y]
+            tile: Tile
+            pygame.draw.rect(game_screen, tile.color,
+                             (x * TILESIZE, y * TILESIZE, TILESIZE, TILESIZE))
+
+
+def render_isometric_tilemap():
+    global game_screen, wall
+    for y in range(MAPWIDTH):
+        for x in range(MAPHEIGH):
+            tile = tilemap[x][y]
+            tile: Tile
+            render_isometric_tile(tile, x, y)
+
+    pygame.display.flip()
+
+
+def render_isometric_tile(tile, x, y):
+    cart_x = x * TILESIZE
+    cart_y = y * TILESIZE
+    iso_x, iso_y = cartesian_to_isometric(cart_x, cart_y)
+    game_screen.blit(tile.texture, (iso_x + border_offset, iso_y + border_offset - wall_height ))
+
+
+def cartesian_to_isometric(x, y):
+    isometric_x = x - y
+    isometric_y = (x + y) / 2
+    return isometric_x, isometric_y
 
 
 def game_loop_handler():
-    render_tilemap()
+    # Different types of rendering
+    # render_basic_tilemap()
+    render_isometric_tilemap()
     pygame.display.update()
-
-
-
-
 
 
 if __name__ == "__main__":
     pygame.init()
-    surface = pygame.display.set_mode((MAPWIDTH * TILESIZE, MAPHEIGH * TILESIZE));
+    game_screen = pygame.display.set_mode(
+        (600, 400))
+
+    wall = load_resource_image("block")
+    floor = load_resource_image("floor")
+
+    tilemap = [
+        [Tile(BROWN, 0, 0, wall), Tile(
+            BROWN, 0, 1, wall), Tile(BROWN, 0, 2, wall)],
+        [Tile(BROWN, 1, 0, wall), Tile(
+            GREEN, 1, 1, floor), Tile(BROWN, 1, 2, wall)],
+        [Tile(BROWN, 2, 0, wall), Tile(
+            GREEN, 2, 1, floor), Tile(BROWN, 2, 2, wall)],
+        [Tile(BROWN, 3, 0, wall), Tile(
+            BROWN, 3, 1, wall), Tile(BROWN, 3, 2, wall)],
+    ]
     while True:
         event = pygame.event.poll()
         if event.type == pygame.QUIT:
@@ -49,4 +103,3 @@ if __name__ == "__main__":
             sys.exit()
 
         game_loop_handler()
-
--- tile.py
+++ tile.py
@@ -2,14 +2,14 @@ import pygame
 
 class Tile:
 
-    def __init__(self, color, x, y):
+    def __init__(self, color, x, y, image=None):
         """
         color - tuple of (r, g, b) or (r, g, b, a)
         x and y are all cartesian coordinates 
         """
         assert len(color) in (3, 4), "type help(Tile) to see constructor variables format"
+        assert isinstance(image, pygame.Surface), image
         self.color = pygame.Color(color)
         self.x = x
         self.y = y
-
-    
+        self.texture = image
